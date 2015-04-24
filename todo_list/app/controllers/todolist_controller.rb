f96b882d4cce618417c2b06e9557114bcbeb0284
Jessica Lombera <jesstlombera@gmail.com>
2015-04-24 16:26:54 -0700

    Fleshed out To Do list

--- todo_list/app/controllers/todolist_controller.rb
+++ todo_list/app/controllers/todolist_controller.rb
@@ -1,4 +1,16 @@
 class TodolistController < ApplicationController
   def index
+  	@todo = Todo.all
+  	render :index
   end
+
+  	def new
+  		render :new
+  	end
+
+  	def create
+  		todo = require[:todo].permit(:name, :description)
+  		Todo.create(todo)
+  		redirect_to '/todo'
+  	end
 end
